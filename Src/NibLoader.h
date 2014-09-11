ac19bfd5bdc44b2ffe839601980e42e01d092825
pjechris <jcpastant@xebia.fr>
2014-09-11 15:36:58 +0200

    project setup

---
+++ Src/NibLoader.h
@@ -0,0 +1,13 @@
+//
+// This file is part of NibLoader
+//
+// Created by JC on 9/11/14.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NibLoader : NSObject
+
+@end
