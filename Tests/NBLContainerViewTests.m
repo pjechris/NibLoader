c842648ab03f7997831c9011bf72fcd4613105fd
pjechris <jcpastant@xebia.fr>
2014-09-12 18:18:33 +0200

    tests

--- Tests/NBLContainerViewTests.m
+++ Tests/NBLContainerViewTests.m
@@ -9,6 +9,7 @@
 
 #import <Foundation/Foundation.h>
 #import "NBLContainerView.h"
+#import "UIView+NibLoader.h"
 
 SPEC_BEGIN(NBLContainerViewTests)
 
@@ -65,19 +66,15 @@ describe(@"with content", ^{
             [[newContentView.superview should] equal:containerView];
             [[containerView.contentView should] equal:newContentView];
         });
-    });
-    
-    context(@"when setting SAME content view", ^{
-        it(@"should do nothing", ^{
-            containerView.contentView = contentView;
-            
-            [[contentView.superview should] equal:containerView];
-        });
-        
-        it(@"should not remove view from container", ^{
-            [[contentView shouldNot] receive:@selector(removeFromSuperview)];
-            
-            containerView.contentView = contentView;
+
+        context(@"when setting SAME content view", ^{
+            it(@"should not remove view", ^{
+                [[contentView shouldNot] receive:@selector(removeFromSuperview)];
+
+                containerView.contentView = contentView;
+
+                [[contentView.superview should] equal:containerView];
+            });
         });
     });
     
@@ -96,15 +93,52 @@ describe(@"with content", ^{
     });
     
     context(@"when adding subview", ^{
-        
-    });
-    
-    context(@"when adding SAME content view", ^{
-        
+        __block UIView *newView;
+
+        beforeEach(^{
+            newView = [UIView new];
+        });
+
+        it(@"should replace content view", ^{
+            [containerView addSubview:newView];
+
+            [[containerView.contentView should] equal:newView];
+        });
+
+        context(@"when adding SAME content view", ^{
+            it(@"should not remove view", ^{
+                [[contentView shouldNot] receive:@selector(removeFromSuperview)];
+
+                [containerView addSubview:contentView];
+
+                [[containerView.contentView should] equal:contentView];
+            });
+        });
     });
     
     describe(@"auto-layout", ^{
+        __block NSDictionary *bindings;
+
+        beforeEach(^{
+            bindings = NSDictionaryOfVariableBindings(containerView, contentView);
+        });
+
         describe(@"when container has constraints", ^{
+            __block UIView *rootView;
+
+            beforeEach(^{
+                rootView = [UIView viewFromNib:[UINib nibWithNibName:@"ViewTests" bundle:[NSBundle bundleForClass:self.class]] owner:nil];
+                containerView = [rootView subviews][0];
+                containerView.contentView = contentView;
+            });
+
+            context(@"contentView size", ^{
+                it(@"should equal container size", ^{
+                    [[theValue(CGRectGetWidth(contentView.frame)) shouldNot] equal:theValue(0)];
+                    [[theValue(CGRectGetWidth(contentView.frame)) should] equal:theValue(CGRectGetWidth(containerView.frame))];
+                });
+            });
+
             context(@"intrinsicContentSize", ^{
                 
             });
